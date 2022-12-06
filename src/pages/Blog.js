6f6616dbb499dd3773d732b6f2edf9db77284f5e
Pitri Indrayani <indrayanipitri@gmail.com>
2022-12-07 01:16:08 +0700

    get api

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -1,7 +1,37 @@
+import { useState, useEffect } from 'react'
+
 export default function Blog() {
+    const [articles, setArticles] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(function() {
+       async function getArticles() {
+            const request = await fetch('https://api.spaceflightnewsapi.net/v3/articles')
+
+            const response = await request.json()
+
+            setArticles(response)
+            setLoading(false)
+            
+       }
+       getArticles();
+    }, [])
+ 
     return (
-        <>
-        <h1>Blog Page</h1>
-        </>
+        <section>
+            <h1>Blog Page</h1>
+            <p>berikut tulisan-tulisan yang aku buat ketika aku sedang sedih : </p>
+
+            {loading ? <i> tunggu ya sist</i> : <div>
+            <p>-- eh tapi bohong hehe..</p>
+            <ul>
+            {articles.map(function(article) {
+                return <li key={article.id}> {article.title}</li>
+            })}
+            </ul>
+                </div> }
+
+            
+        </section>
     )
 }
